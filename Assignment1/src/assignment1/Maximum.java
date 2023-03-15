ddf6c3602a60adb2d6cd09e513234a6ee2025ff6
RichaSingh4 <100857253+RichaSingh4@users.noreply.github.com>
2023-03-15 18:29:10 +0530

    Update Maximum.java

--- Assignment1/src/assignment1/Maximum.java
+++ Assignment1/src/assignment1/Maximum.java
@@ -4,18 +4,27 @@ import java.util.Scanner;
 
 public class Maximum {
 	public static void main(String[] args) {
+		
 		Scanner sc=new Scanner(System.in);
+		//taking input from user...
 		System.out.println("enter length of array");
 		int n=sc.nextInt();
 		int[]arr=new int[n];
+		
+		//taking input from user...
 		System.out.println("enter element of array");
 		for(int i=0;i<n;i++) {
+			//storing value in array..
 			arr[i]=sc.nextInt();
 		}
+		
+		//printing and calling max method....
 		System.out.println(max(arr));
 	sc.close();
 
 	}
+	
+	//method to find maximum element of array....
 	public static int max(int[] arr) {
 		int max=Integer.MIN_VALUE;
 		for(int i=0;i<arr.length;i++) {
